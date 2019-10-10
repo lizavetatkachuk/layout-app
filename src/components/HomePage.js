07a7b258f860f3898ce03fb09b59cbc994e02165
lizavetatkachuk <elis.tkachuk@gmail.com>
2019-10-10 11:26:30 +0300

    Vertical text centered

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,42 +1,43 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const Container = styled.div`
-	display: flex;
+  display: flex;
 `;
 
 const SideBar = styled.div`
-	display: flex;
-	align-items: center;
-	background-color: black;
-	color: #e3b873;
-	height: 100vh;
-	width: 80px;
+  display: flex;
+  align-items: center;
+  background-color: black;
+  color: #e3b873;
+  height: 100vh;
+  width: 80px;
 `;
 
 const VerticalText = styled.div`
-	display: flex;
-	align-items: center;
-	transform: rotate(90deg);
-	text-transform: uppercase;
+  display: flex;
+  align-items: baseline;
+  margin: auto;
+  writing-mode: vertical-rl;
+  text-transform: uppercase;
 `;
 const Line = styled.div`
-	height: 2px;
-	width: 60px;
-	margin-left: 10px;
-	background-color: #e3b873;
+  height: 60px;
+  width: 2px;
+  margin-left: 10px;
+  background-color: #e3b873;
 `;
 
 const HomePage = () => {
-	return (
-		<Container>
-			<SideBar>
-				<VerticalText>
-					<div>Instagram </div>
-					<Line />
-				</VerticalText>
-			</SideBar>
-		</Container>
-	);
+  return (
+    <Container>
+      <SideBar>
+        <VerticalText>
+          <div>Instagram </div>
+          <Line />
+        </VerticalText>
+      </SideBar>
+    </Container>
+  );
 };
 export default HomePage;
